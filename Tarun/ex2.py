1d67320ce0d7ebf9c83c9336eaedfcecfba102ec
Tarster <sharmatarun537@gmail.com>
2021-07-31 21:59:57 +0530

    Exercise 2 complete

---
+++ Tarun/ex2.py
@@ -0,0 +1,15 @@
+# A comment, thi is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") #and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+# Question 1
+# Find out if you were right about what the # character does and make sure 
+# you know what it’s called (octothorpe or pound character).
+# https://www.mentalfloss.com/article/64280/where-does-hashtag-aka-octothorpe-come
+
