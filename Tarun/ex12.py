d7e4c1e9e4527239715a3d5829bef9204f8e0906
Tarster <sharmatarun537@gmail.com>
2021-08-01 23:51:09 +0530

    Fixed a typo
    exercise added for inputs with inline statement

---
+++ Tarun/ex12.py
@@ -0,0 +1,16 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight =input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# Help on built-in function input in module builtins:
+
+# input(prompt=None, /)
+#     Read a string from standard input.  The trailing newline is stripped.
+
+#     The prompt string, if given, is printed to standard output without a
+#     trailing newline before reading input.
+
+#     If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise EOFError.     
+#     On *nix systems, readline is used if available.
